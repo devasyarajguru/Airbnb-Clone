810f3711437c4927bc9fd00c29b6dd4044a50fce
Devasya <devasyarajguru@gmail.com>
2023-10-06 09:59:54 +0530

    airbnb clone

---
+++ airbnb/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from '../images/img1.jpg';
+import '../App.css'
+
+export default function Navbvar()
+{
+    return(
+        <>
+           <nav>
+                <img src={logo}></img>
+           </nav>
+        </>
+    )
+}
