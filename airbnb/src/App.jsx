ed6e965ad2e0927e293ccbd246b9fc66f4cd57a4
Devasya <devasyarajguru@gmail.com>
2023-10-11 07:20:02 +0530

    airbnb clone

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -2,13 +2,25 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import './App.css'
 import Card from './components/Card';
+import Data from './data';
 export default function App()
 {
+  const myCard = Data.map(data => {
+      return <Card 
+                  key={data.id}
+                  img={data.coverImg}
+                  title={data.title}
+                  price={data.price}
+                  rating={data.stats.rating}
+                  location={data.location}
+              />
+  })
   return(
+    
     <>
         <Navbar />
         <Hero />
-        <Card />
+        {myCard}
     </>
   )
 }
--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -1,17 +1,26 @@
-import card1 from "../images/card-img1.jpg";
+// import card1 from "../images/card-img1.jpg";
+import PropTypes from 'prop-types';
 import StarIcon from '@mui/icons-material/Star';
-export default function Card()
+export default function Card(props)
 {
+    Card.propTypes ={
+        price:PropTypes.number.isRequired,
+        img:PropTypes.string.isRequired,
+        rating:PropTypes.number.isRequired,
+        title:PropTypes.string.isRequired,
+        location:PropTypes.string.isRequired
+    }
     return(
         <>
             <section className="card--section">
                 <div className="card--container">
-                    <img src={card1} className="card-1"/>
+                    <img src={`../images/${props.img}`} className="card-1"/>
+                    
                     <div className="ratings">
-                       <StarIcon color="warning" /> <span className="rating-number">5.0 <span className="rating-number-span">(6) - USA</span></span>
+                       <StarIcon color="warning" /> <span className="rating-number">{props.rating} <span className="rating-number-span">(6) - USA</span></span>
                     </div>
-                    <span className="card-details">Life Lessons with Katie</span><br></br>
-                    <span className="card-price"><span className="card-price-span">From $136 /</span> person</span>
+                    <span className="card-details">{props.title}</span><br></br>
+                    <span className="card-price"><span className="card-price-span">From {props.price} /</span>    person</span>
                 </div>
             </section>
         </>
